8d1f740d58ece9617314475f707944e7d5bc62d8
Homeless Aid UK <admin@homelessaid.co.uk>
2025-09-07 22:36:51 +0100

    Remove test endpoint to stay under 12 function limit

--- api/test-endpoint.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    return res.status(200).json({ 
-        message: 'Endpoint working!', 
-        method: req.method,
-        timestamp: new Date().toISOString()
-    });
-};
